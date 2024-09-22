03a8f5d514ad6fc7d5166f0939e477a5cf17e320
cachume <albertq703@gmail.com>
2024-09-22 19:33:33 -0400

    Cambios en MenuPrincipal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from interfaces.pantallaPrincipal import MainScreen
 from interfaces.pantallaRegistro import Registro
 from models.systemdb import Pydb
-from interfaces.pantallaLeds import Leds
-from interfaces.pantallauleds import LedsU
 from tkinter import *
 from tkinter import ttk, messagebox
 
@@ -13,31 +11,35 @@ class MainApp(Tk):
     def __init__(self):
         super().__init__()  
         self.title("Sistema Proyecto")
-        self.geometry("500x500")
-        self.config(bg="#f0f0f0")   
-
+        self.geometry("800x450")
+        self.config(bg="#ffffff")   
+        self.frameone = Frame(self,bg="#cab4a0")
+        self.iconHome = PhotoImage(file="assets/home.png")
         self.setup_ui()
 
     def setup_ui(self):
-        Label(self, text="Iniciar Sesión", font=("Helvetica", 24, 'bold'), bg="#f0f0f0", fg="#333").pack(anchor=CENTER, pady=20)
-        Label(self, text="Introduce tus datos", font=("Helvetica", 12), bg="#f0f0f0", fg="#666").pack(anchor=CENTER, pady=5)
+        self.frameone.place(x=0,y=0,width=350,height=450)
+        Label(self.frameone, image=self.iconHome,bg="#cab4a0").pack(pady=80)
+        Label(self.frameone, text="SMART HOME", font=("Helvetica", 24, 'bold'), bg="#cab4a0", fg="#000000").pack()
+        Label(self, text="Bienvenido Nuevamente", font=("Helvetica", 24, 'bold'), bg="#ffffff", fg="#000000").place(y=40,x=390)
+        Label(self, text="Introduce tus datos", font=("Helvetica", 12,'bold'), bg="#ffffff", fg="#000000").place(y=90,x=500)
 
-        self.label_correo = Label(self, text="Correo Electrónico:", font=("Helvetica", 10), bg="#f0f0f0", fg="#333")
-        self.entry_correo = Entry(self, width=30, font=("Helvetica", 10))
+        self.label_correo = Label(self, text="Correo Electrónico:", font=("Helvetica", 10,'bold'), bg="#ffffff", fg="#000000")
+        self.entry_correo = Entry(self, width=35, font=("Helvetica", 10), highlightbackground="#000000",highlightthickness=1)
 
-        self.label_contrasena = Label(self, text="Contraseña:", font=("Helvetica", 10), bg="#f0f0f0", fg="#333")
-        self.entry_contrasena = Entry(self, width=30, font=("Helvetica", 10), show="*")
+        self.label_contrasena = Label(self, text="Contraseña:", font=("Helvetica", 10, 'bold'), bg="#ffffff", fg="#000000")
+        self.entry_contrasena = Entry(self, width=35, font=("Helvetica", 10), show="*", highlightbackground="#000000",highlightthickness=1)
 
-        self.label_correo.place(y=200, x=60)
-        self.entry_correo.place(y=202, x=230)
-        self.label_contrasena.place(y=230, x=60)
-        self.entry_contrasena.place(y=232, x=230)
+        self.label_correo.place(y=170, x=440)
+        self.entry_correo.place(y=202, x=450)
+        self.label_contrasena.place(y=230, x=440)
+        self.entry_contrasena.place(y=260, x=450)
 
-        self.boton_login = Button(self, text='Iniciar Sesión', bg="#007acc", fg="#fff", font=('Helvetica', 12, 'bold'), command=self.auth_user)
-        self.boton_registro = Button(self, text='Registrarse', bg="#f0f0f0", fg="#007acc", font=('Helvetica', 12, 'bold'), command=self.registro)
+        self.boton_login = Button(self, text='Iniciar Sesión', bg="#755f50", fg="#ffffff", font=('Helvetica', 12, 'bold'), command=self.auth_user)
+        self.boton_registro = Button(self, text='Registrarse', bg="#755f50", fg="#ffffff", font=('Helvetica', 12, 'bold'), command=self.registro)
         
-        self.boton_login.place(y=350, x=120)
-        self.boton_registro.place(y=350, x=260)
+        self.boton_login.place(y=350, x=440)
+        self.boton_registro.place(y=350, x=590)
 
     def auth_user(self):
         correo = self.entry_correo.get()
